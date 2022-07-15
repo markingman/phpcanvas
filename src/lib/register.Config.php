428424acb6aa9dc70ebec9bd12ba96fd59753123
Mark Ingman <mark.ingman@valhalla.software>
2022-07-15 12:39:09 +0100

    Tidy up

--- src/lib/register.Config.php
+++ src/lib/register.Config.php
@@ -1,4 +1,4 @@
-<?php // $Id: register.Config.php 720 2017-09-07 12:56:50Z dev $
+<?php
 
 return function ($c) use ($config_paths, $config) {
 	$Config = new PHPCanvas\Config($config_paths, $config);
