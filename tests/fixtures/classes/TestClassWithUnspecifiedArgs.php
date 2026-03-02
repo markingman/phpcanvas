397473442af8f9967cfd866cf0a05c24349547c1
Mark Ingman <mark.ingman@valhalla.software>
2026-03-02 06:12:11 +0000

    Tidy up

--- tests/fixtures/classes/TestClassWithUnspecifiedArgs.php
+++ tests/fixtures/classes/TestClassWithUnspecifiedArgs.php
@@ -4,7 +4,7 @@ namespace PHPCanvas\Test;
 
 class TestClassWithUnspecifiedArgs
 {
-	/** @phpstan-ignore-next-line */
+	// @phpstan-ignore-next-line
 	public function test($z): string
 	{
 		return is_bool($z) ? 'bool' : 'not-bool';
